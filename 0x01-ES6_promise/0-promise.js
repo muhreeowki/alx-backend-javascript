a179f5174c41ea260226d1b6e69eacd837119064
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-04-10 10:06:21 +0300

    Task 0: Fixing synatx error

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,4 @@
-export function getResponseFromAPI() {
-  return new Promise();
+/* eslint-disable */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {});
 }
