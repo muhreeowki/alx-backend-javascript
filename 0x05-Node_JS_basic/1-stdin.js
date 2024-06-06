48e48a45ceacd5fd6ebc5335dac3786b000d6c72
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-06-06 13:21:29 +0300

    Added Tasks

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,16 @@
-const readline = require("node:readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-rl.question("Welcome to Holberton School, what is your name?\n", (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log("This important software is now closing");
-  rl.close();
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
